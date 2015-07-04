1c6eeeb3f1ee7055f5d2e80e580b0b78b5eb99e6
HEnG <heng4786@gmail.com>
2015-07-04 20:44:53 +0800

    add calendar html framework

--- jquery.calendar.js
+++ jquery.calendar.js
@@ -1,21 +1,38 @@
+
 ; (function (root, factory) {
     if (typeof define === "function" && (define.amd || define.cmd)) {
         define(function (require, exports, module) {
-            return factory(root);
+            return factory(root, jQuery);
         });
     } else {
-        factory(root);
+        factory(root, jQuery);
     }
-})(window, function (root) {
+})(window, function (root, $) {
 
     var defaults = {
-        
+
     };
 
     jQuery.fn.extend({
         calendar: function (options) {
             var opts = $.extend({}, defaults, options);
             console.log(opts);
+            var $cWrap = $("<div class='calendar'></div>");
+            var $cHead = $(
+                "<div class='head'>\
+                    <div class='primary'>\
+                        <span class='year'>2015</span>年\
+                        <span class='month'>05</span>月\
+                    </div>\
+                    <div class='secondary'></div>\
+                </div>"
+                );
+            var $cmain = $("<div class='main'></div>");
+            var $cFoot = $("<div class='foot'></div>");
+            
+            $cWrap.append($cHead, $cmain, $cFoot);
+            
+            $cWrap.appendTo("body");
         }
     });
 
