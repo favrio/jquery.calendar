0f5f955071b8309819655890ed3a24fd014b038c
HEnG <heng4786@gmail.com>
2015-07-04 21:01:47 +0800

    add basic styles cellBuilder

--- jquery.calendar.js
+++ jquery.calendar.js
@@ -10,13 +10,27 @@
 })(window, function (root, $) {
 
     var defaults = {
-
+        cellSize: 42
     };
+    
+    // 工具函数
+    
+    // 日历格子生成器
+    function cellBuilder(size) {
+        var rt = "<ul>", i;
+        for (i = 0; i < size; i++) {
+            rt += "<li></li>";
+        }
+        rt += "</ul>";
+        return rt;
+    }
 
     jQuery.fn.extend({
         calendar: function (options) {
             var opts = $.extend({}, defaults, options);
             console.log(opts);
+            
+            // 初始元素 
             var $cWrap = $("<div class='calendar'></div>");
             var $cHead = $(
                 "<div class='head'>\
@@ -24,14 +38,29 @@
                         <span class='year'>2015</span>年\
                         <span class='month'>05</span>月\
                     </div>\
-                    <div class='secondary'></div>\
+                    <div class='secondary'>\
+                        <ul class='weeks'>\
+                            <li>日</li>\
+                            <li>一</li>\
+                            <li>二</li>\
+                            <li>三</li>\
+                            <li>四</li>\
+                            <li>五</li>\
+                            <li>六</li>\
+                        </ul>\
+                    </div>\
                 </div>"
                 );
             var $cmain = $("<div class='main'></div>");
             var $cFoot = $("<div class='foot'></div>");
-            
+
+            var cellStr = cellBuilder(opts.cellSize);
+            $cmain.html(cellStr);
+
+
+
             $cWrap.append($cHead, $cmain, $cFoot);
-            
+
             $cWrap.appendTo("body");
         }
     });
