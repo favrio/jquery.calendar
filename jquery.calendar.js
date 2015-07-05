aaaa68177980ee2414ac1810d0443606e921e30a
HEnG <heng4786@gmail.com>
2015-07-05 10:23:30 +0800

    add some tool function

--- jquery.calendar.js
+++ jquery.calendar.js
@@ -9,14 +9,26 @@
 	}
 })(window, function(root, $) {
 
+	// 初始配置
 	var defaults = {
 		cellSize: 42,
 		wrapClass: "calendar"
 	};
 
+	// 元素引用
+	var $cWrap,
+		$cHead,
+		$cmain,
+		$cFoot;
+
+
 	// 工具函数
 
-	// 日历格子生成器
+	/**
+	 * 日历格子生成器
+	 * @param  {Number} size 需要生成的格子数量
+	 * @return {String}      格子列表字html符串
+	 */
 	function cellBuilder(size) {
 		var rt = "<ul>",
 			i;
@@ -27,12 +39,67 @@
 		return rt;
 	}
 
-	// 定位设置 
+	/**
+	 * 日历定位设置
+	 * @param {jQuery Object} $el 需要设置定位的元素
+	 * @param {Number} x   X坐标，基于整个页面
+	 * @param {Number} y   y坐标，基于整个页面
+	 */
 	function setPosi($el, x, y) {
 		$el.css({
 			left: x || 0,
 			top: y || 0
 		});
+	}
+
+	/**
+	 * 判断是不是闰年
+	 * @param  {Number}  year 年份
+	 * @return {Boolean}      是或者不是闰年
+	 */
+	function isLeapYear(year) {
+		return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)
+	}
+
+	/**
+	 * 一位数补齐，小于2位的自动补齐为2位
+	 * @param  {Number} n 任意数字
+	 * @return {String}   补齐后的数字字符串
+	 */
+	function getTwoBit(n) {
+		return (n > 9 ? "" : "0") + n
+	}
+
+	/*
+	 * 日期对象转成字符串
+	 * @param  {Date} new Date()
+	 * @split  {String} "-" 或 "/"
+	 * @return {String} "2014-12-31"
+	 */
+	function date2Str(date, split) {
+		split = split || "-";
+		var y = date.getFullYear();
+		var m = getTwoBit(date.getMonth() + 1);
+		var d = getTwoBit(date.getDate());
+		return [y, m, d].join(split);
+	}
+
+	/**
+	 * 字符串转Date对象
+	 * @param  {String} str 日期字符串
+	 * @return {Date}     日期对象
+	 */
+	function str2Date(str) {
+		var reDate = /^\d{4}\-\d{1,2}\-\d{1,2}/;
+		if (reDate.test(str)) {
+			str = str.replace(/-/g, "/");
+			/* 
+			直接通过"-"在日期构造器里面使用，会出现兼容性问题
+			http://my.oschina.net/epstar/blog/289949
+			http://www.cnblogs.com/snandy/p/3992443.html
+			*/
+		}
+		return new Date(str);
 	}
 
 
@@ -43,8 +110,8 @@
 			var offset = $input.offset();
 
 			// 初始元素 
-			var $cWrap = $("<div class='" + opts.wrapClass + "'></div>");
-			var $cHead = $(
+			$cWrap = $("<div class='" + opts.wrapClass + "'></div>");
+			$cHead = $(
 				"<div class='head'>\
 					<div class='primary'>\
 						<span class='year'>2015</span>年\
@@ -63,8 +130,8 @@
 					</div>\
 				</div>"
 			);
-			var $cmain = $("<div class='main'></div>");
-			var $cFoot = $("<div class='foot'></div>");
+			$cmain = $("<div class='main'></div>");
+			$cFoot = $("<div class='foot'></div>");
 
 			// 填充日历格子，数量可控 
 			var cellStr = cellBuilder(opts.cellSize);
@@ -81,14 +148,13 @@
 				}
 			}
 
-
-			$cWrap.append($cHead, $cmain, $cFoot).hide();
-
-			$cWrap.appendTo("body");
+			// 组合Calendar，并隐藏放到body下
+			$cWrap.append($cHead, $cmain, $cFoot).hide().appendTo("body");
+			// 设置日历的定位
 			setPosi($cWrap, opts.x || offset.left, opts.y || offset.top + $input.outerHeight());
 
+			// 事件绑定
 			$input.on("click focus", handles.show);
-
 			$(document).on("click", function(ev) {
 				if (!$(ev.target).closest("." + opts.wrapClass).length) {
 					handles.hide();
